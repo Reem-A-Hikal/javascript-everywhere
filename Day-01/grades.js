3410d8575ccb493023ec7e200651cea24274022b
Reem Atef <reemhikal19@gmail.com>
2026-09-12 16:43:52 +0300

    Replace for..of with forEach; add highest score
    Commented out the original for..of loop and replaced it with grades.forEach

--- Day-01/grades.js
+++ Day-01/grades.js
@@ -12,7 +12,24 @@ let excellentCount = 0;
 let goodCount = 0;
 let needsWorkCount = 0;
 
-for (const student of grades) {
+// for (const student of grades) {
+//   if (student.score >= 90) {
+//     console.log(`${student.name}: ${student.score} → Excellent`);
+//     excellentCount++;
+//   } else if (student.score >= 70 && student.score <= 89) {
+//     console.log(`${student.name}: ${student.score} → Good`);
+//     goodCount++;
+//   } else if (student.score < 70) {
+//     console.log(`${student.name}: ${student.score} → Needs work`);
+//     needsWorkCount++;
+//   }
+// }
+
+// console.log(
+//   `Summary: ${excellentCount} Excellent, ${goodCount} Good, ${needsWorkCount} Needs work.`,
+// );
+
+grades.forEach((student) => {
   if (student.score >= 90) {
     console.log(`${student.name}: ${student.score} → Excellent`);
     excellentCount++;
@@ -23,8 +40,18 @@ for (const student of grades) {
     console.log(`${student.name}: ${student.score} → Needs work`);
     needsWorkCount++;
   }
-}
+});
 
 console.log(
   `Summary: ${excellentCount} Excellent, ${goodCount} Good, ${needsWorkCount} Needs work.`,
 );
+
+const newGrades = [
+  { name: "Hana", score: 95 },
+  { name: "Youssef", score: 82 },
+  { name: "Nour", score: 67 },
+  { name: "Rania", score: 91 }
+];
+
+let theHighestScore = Math.max(...newGrades.map(student => student.score));
+console.log(`The highest score is: ${theHighestScore}`);
